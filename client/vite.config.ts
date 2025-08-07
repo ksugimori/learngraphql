10cee6df8ec1881d82ae3ce5ca6abd9c411c54dc
ksugimori <12605906+ksugimori@users.noreply.github.com>
2025-08-07 22:30:19 +0900

    setup eslint plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({ babel: { plugins: ["relay"] } })],
+  plugins: [
+    react({ babel: { plugins: ["relay"] } }),
+    eslint({
+      failOnError: false,
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+    }),
+  ],
 });
